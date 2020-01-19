234fb3dbcd9ae3138b48f35f7a3aefecf7829ff8
azharyarliansyah <azhary.arliansyah@mncgroup.com>
2020-01-20 00:28:55 +0700

    refine footer menu

--- components/Includes/Schedules/LiveTv.js
+++ components/Includes/Schedules/LiveTv.js
@@ -7,9 +7,9 @@ import '../../../assets/scss/components/schedule-livetv.scss';
 export default class LiveTv extends React.Component {
     render() {
         return (
-                <div className="schedule-livetv">
-                    <h3>i News Live Streaming</h3>
-                </div>
-                ); 
+            <div className="schedule-livetv">
+                <h3>i News Live Streaming</h3>
+            </div>
+        );
     }
 }
