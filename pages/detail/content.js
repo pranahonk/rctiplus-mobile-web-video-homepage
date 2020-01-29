3186051dd77393d1a46d2a1a4a78084e48974af2
azharyarliansyah <azhary.arliansyah@mncgroup.com>
2020-01-29 14:45:13 +0700

    added detail content page metadata for SEO

--- pages/detail/content.js
+++ pages/detail/content.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import Head from 'next/head';
 import { connect } from 'react-redux';
 import Router, { withRouter } from 'next/router';
+import fetch from 'isomorphic-unfetch';
+
 import initialize from '../../utils/initialize';
 import contentActions from '../../redux/actions/contentActions';
 import historyActions from '../../redux/actions/historyActions';
@@ -11,11 +14,46 @@ import Layout from '../../components/Layouts/Default';
 
 import '../../assets/scss/components/content.scss';
 
+import { DEV_API, VISITOR_TOKEN, SITE_NAME } from '../../config';
+import { getCookie } from '../../utils/cookie';
+
 class Content extends React.PureComponent {
 
-    static getInitialProps(ctx) {
-		initialize(ctx);
-		return { context_data: ctx.query };
+    static async getInitialProps(ctx) {
+        initialize(ctx);
+        
+        const accessToken = getCookie('ACCESS_TOKEN');
+        const res = await fetch(`${DEV_API}/api/v1/${ctx.query.type}/${ctx.query.content_id}/url`, {
+            method: 'GET',
+            headers: {
+                'Authorization': accessToken ? accessToken : VISITOR_TOKEN
+            }
+        });
+
+        const error_code = res.statusCode > 200 ? res.statusCode : false;
+        const data = await res.json();
+        if (error_code || data.status.code != 0) {
+            return { initial: false, content_url: {}, content: {} };
+        }
+
+        const res_2 = await fetch(`${DEV_API}/api/v1/${ctx.query.type}/${ctx.query.content_id}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': accessToken ? accessToken : VISITOR_TOKEN
+            }
+        });
+
+        const error_code_2 = res_2.statusCode > 200 ? res_2.statusCode : false;
+        const data_2 = await res_2.json();
+        if (error_code_2 || data_2.status.code != 0) {
+            return { initial: false, content_url: {}, content: {} };
+        }
+
+		return { 
+            context_data: ctx.query, 
+            content_url: data,
+            content: data_2
+        };
 	}
 
     constructor(props) {
@@ -81,33 +119,28 @@ class Content extends React.PureComponent {
                     });
             }, 10000);
         });
+    }
+    
+    getMetadata() {
+        let metadata = {
+            title: `${this.props.content_url.data.content_name} - ${SITE_NAME}`,
+            description: ''
+        };
+        switch (this.props.context_data.type) {
+            case 'episode':
+                metadata.description = `Nonton ${this.props.content_url.data.content_name} Online - Season ${this.props.content.data.season} - Episode ${this.props.content.data.episode} - RCTI+`;
+                break;
 
-        
-	}
+            case 'extra':
+            case 'clip':
+                metadata.description = `${this.props.content_url.data.content_name} - ${this.props.content_url.data.program_title} - ${SITE_NAME}`;
+                break;
+        }
+        return metadata;
+    }
 
     async componentDidMount() {
-        let response = null;
-        try {
-            switch (this.props.context_data.type) {
-                case 'episode':
-                    response = await this.props.getEpisodeUrl(this.props.context_data.content_id);
-                    break;
-
-                case 'extra':
-                    response = await this.props.getExtraUrl(this.props.context_data.content_id);
-                    break;
-                    
-                case 'clip':
-                    response = await this.props.getClipUrl(this.props.context_data.content_id);
-                    break;
-            }    
-        }
-        catch (error) {
-            console.log(error);
-        }
-        
-        if (response && response.status === 200 && response.data.status.code === 0) {
-            const data = response.data.data;
+        if (this.props.content_url && this.props.content_url.status.code === 0) {
             this.props.getContinueWatchingByContentId(this.props.context_data.content_id, this.props.context_data.type)
                 .then(response_2 => {
                     let startDuration = 0;
@@ -116,15 +149,15 @@ class Content extends React.PureComponent {
                     }
 
                     this.setState({
-                        player_url: data.url,
-                        player_vmap: data.vmap,
+                        player_url: this.props.content_url.data.url,
+                        player_vmap: this.props.content_url.data.vmap,
                         start_duration: startDuration
                     }, () => this.initVOD());
                 })
                 .catch(error => {
                     this.setState({
-                        player_url: data.url,
-                        player_vmap: data.vmap,
+                        player_url: this.props.content_url.data.url,
+                        player_vmap: this.props.content_url.data.vmap,
                         start_duration: 0
                     }, () => this.initVOD());
                 });
@@ -133,8 +166,13 @@ class Content extends React.PureComponent {
     }
 
     render() {
+        const metadata = this.getMetadata();
+
         return (
-            <Layout title="RCTI+ - Live Streaming Program 4 TV Terpopuler">
+            <Layout title={metadata.title}>
+                <Head>
+                    <meta name="description" content={metadata.description}/>
+                </Head>
                 <ArrowBackIcon className="back-btn" onClick={() => Router.back()}/>
                 <div className="player-container">
                     <div id="app-jwplayer"></div>
