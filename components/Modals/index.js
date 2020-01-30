6ca82a540d2d706393e7eeb90b4eeb615275cbfc
azharyarliansyah <azhary.arliansyah@mncgroup.com>
2020-01-30 10:35:26 +0700

    test wording player setup error

--- components/Modals/index.js
+++ components/Modals/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
 import ReactJWPlayer from 'react-jw-player';
 
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import pageActions from '../../redux/actions/pageActions';
 
-import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+
+import { Modal, ModalHeader, ModalBody, Button } from 'reactstrap';
 
 import '../../assets/scss/components/modal.scss';
 
@@ -13,21 +15,60 @@ class PlayerModal extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            error: false,
+            error_data: {}
+        };
+    }
+
+    tryAgain() {
+        this.props.setPageLoader();
+        this.setState({ error: false }, () => {
+            this.props.unsetPageLoader();
+        });
     }
 
     render() {
+        let playerRef = (<ReactJWPlayer 
+            playerId={this.props.playerId} 
+            isAutoPlay={false}
+            onReady={this.props.onReady}
+            playerScript="https://cdn.jwplayer.com/libraries/Vp85L1U1.js"
+            onSetupError={error => {
+                console.log(error);
+                this.setState({
+                    error: true,
+                    error_data: error
+                });
+            }}
+            file={this.props.videoUrl}/>);
+        if (this.state.error) {
+            playerRef = (
+                <div className="wrapper-content" style={{ margin: 0 }}>
+                    <div style={{ 
+                        textAlign: 'center',
+                        position: 'fixed', 
+                        top: '50%', 
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)' 
+                        }}>
+                        <SentimentVeryDissatisfiedIcon style={{ fontSize: '4rem' }}/>
+						<h5>
+							<strong>{this.state.error_data.message}</strong><br/><br/>
+							<Button onClick={this.tryAgain.bind(this)} className="btn-next block-btn">Coba Lagi</Button>
+						</h5>
+					</div>
+                </div>
+            );
+        }
+
         return (
             <Modal isOpen={this.props.open} toggle={this.props.toggle}>
                 <ModalHeader toggle={this.props.toggle}>
                     <ArrowBackIcon onClick={this.props.toggle}/>
                 </ModalHeader>
                 <ModalBody>
-                    <ReactJWPlayer 
-                        playerId={this.props.playerId} 
-                        isAutoPlay={false}
-                        onReady={this.props.onReady}
-                        playerScript="https://cdn.jwplayer.com/libraries/Vp85L1U1.js"
-                        file={this.props.videoUrl}/>
+                    {playerRef}
                 </ModalBody>
             </Modal>
         );
@@ -35,4 +76,4 @@ class PlayerModal extends React.Component {
 
 }
 
-export default connect(state => state, {})(PlayerModal);
+export default connect(state => state, pageActions)(PlayerModal);
