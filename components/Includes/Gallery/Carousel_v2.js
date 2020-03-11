2035028ead45d5b3dd1516ab16824ca5595190bd
azharyarliansyah <azhary.arliansyah@mncgroup.com>
2020-03-11 11:15:58 +0700

    bug fix square banner

--- components/Includes/Gallery/Carousel_v2.js
+++ components/Includes/Gallery/Carousel_v2.js
@@ -90,7 +90,7 @@ class Crs_v2 extends Component {
                             }}>
                                 <Img 
                                     alt={b.title}
-                                    src={[`${this.state.meta.image_path + this.state.resolution + b.square_image}`, '/static/placeholders/placeholder_potrait.png']}
+                                    src={[`${this.state.meta.image_path + this.state.resolution + b.square_image}`, '/static/placeholders/placeholder_landscape.png']}
                                     unloader={<img src="/static/placeholders/placeholder_landscape.png"/>}
 									loader={<img src="/static/placeholders/placeholder_landscape.png"/>}/>
                             </div>
