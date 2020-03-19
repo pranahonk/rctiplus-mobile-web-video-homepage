849be70d75f20403aa34a7c50712d7d9bf9dc35d
azharyarliansyah <azhary.arliansyah@mncgroup.com>
2020-03-19 15:35:21 +0700

    added query string for mobile app

--- components/Includes/Navbar/NavTrending_v2.js
+++ components/Includes/Navbar/NavTrending_v2.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Router from 'next/router';
+import Router, { withRouter } from 'next/router';
 import { connect } from 'react-redux';
 import Link from 'next/link';
 
 import actions from '../../../redux/actions';
 import pageActions from '../../../redux/actions/pageActions';
 
-import { getCookie, removeCookie } from '../../../utils/cookie';
+import { getCookie, removeCookie, removeAccessToken, setAccessToken } from '../../../utils/cookie';
 import '../../../assets/scss/components/navbar.scss';
 
 //load reactstrap
@@ -17,6 +17,8 @@ import SearchIcon from '@material-ui/icons/Search';
 
 import { newsGeneralEvent } from '../../../utils/appier';
 
+import queryString from 'query-string';
+
 class NavTrending extends Component {
     constructor(props) {
         super(props);
@@ -24,6 +26,23 @@ class NavTrending extends Component {
             token: getCookie('ACCESS_TOKEN'),
             is_top: true
         };
+        
+        this.accessToken = null;
+        this.platform = null;
+        const segments = this.props.router.asPath.split(/\?/);
+        if (segments.length > 1) {
+            const q = queryString.parse(segments[1]);
+            if (q.token) {
+                this.accessToken = q.token;
+                setAccessToken(q.token);
+            }
+            if (q.platform) {
+                this.platform = q.platform;
+            }
+        }
+        else {
+            removeAccessToken();
+        }
     }
 
     signOut() {
@@ -79,13 +98,13 @@ class NavTrending extends Component {
                         <div className="right-top-link">
                             {this.state.token ? (
                                 <div className="btn-link-top-nav">
-                                    <Link href="/trending/search">
+                                    <Link href={"/trending/search" + `${this.accessToken ? `?token=${this.accessToken}&platform=${this.platform}` : ''}`}>
                                         <SearchIcon style={{fontSize: 20, marginRight: 10}}/>
                                     </Link>
                                 </div>
                             ) : (
                                 <div className="btn-link-top-nav">
-                                    <NavbarBrand style={{color: 'white'}} href="/trending/search_v2">
+                                    <NavbarBrand style={{color: 'white'}} href={"/trending/search_v2" + `${this.accessToken ? `?token=${this.accessToken}&platform=${this.platform}` : ''}`}>
                                         <SearchIcon style={{fontSize: 20}}/>
                                     </NavbarBrand>
                                 </div>
@@ -100,4 +119,4 @@ class NavTrending extends Component {
 export default connect(state => state, {
     ...actions,
     ...pageActions
-})(NavTrending);
+})(withRouter(NavTrending));
