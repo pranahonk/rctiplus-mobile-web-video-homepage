cc69753747f8f3b7f0f4e6144e28fdcd4b5f387b
azharyarliansyah <azhary.arliansyah@mncgroup.com>
2020-03-20 18:31:56 +0700

    bug fix iframe

--- pages/trending/detail_v2.js
+++ pages/trending/detail_v2.js
@@ -231,7 +231,7 @@ class Detail extends React.Component {
                     {this.renderActionButton()}
                 </div>
 
-                {this.state.iframe_opened ? (<iframe src={cdata.link} style={{ width: '100%', minHeight: 'calc(100vh - 50px)', paddingTop: 65 }} frameBorder="0" type="text/html"></iframe>) : (
+                {this.state.iframe_opened ? (<iframe src={cdata.link} style={{ width: '100%', minHeight: (this.platform && (this.platform == 'android' || this.platform == 'ios') ? 'calc(100vh)' : 'calc(100vh - 50px)'), paddingTop: 65 }} frameBorder="0" type="text/html"></iframe>) : (
                     <ScrollPercentage onChange={(percentage) => {
                         if (percentage > 0.32) {
                             if (!this.state.scrolled_down) {
