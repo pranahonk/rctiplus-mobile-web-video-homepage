4ab6d2499159f9de749423b8da1a43c192fa9ae4
helmirofiqi <>
2020-05-14 05:36:12 +0700

    -defer script -minimize fontawesome -minimize conviva

--- components/Layouts/Default_v2.js
+++ components/Layouts/Default_v2.js
@@ -104,12 +104,12 @@ class Default_v2 extends React.Component {
                     {/* <script data-ad-client="ca-pub-7595624984434758" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> */}
 
 
-                    <script src="/static/js/fontawesome.js" crossOrigin="anonymous"></script>
+                    <script src="/static/js/fontawesome.js" crossOrigin="anonymous" defer></script>
 
-                    <script src="https://cdn.jwplayer.com/libraries/Vp85L1U1.js"></script>
-                    <script dangerouslySetInnerHTML={{ __html: `jwplayer.key = "Mh/98M9sROX0uXhFlJwXZYeCxbJD5E1+e2goFcRZ07cI/FTu";` }}></script>
+                    {/*<script src="https://cdn.jwplayer.com/libraries/Vp85L1U1.js"></script>
+                    <script dangerouslySetInnerHTML={{ __html: `jwplayer.key = "Mh/98M9sROX0uXhFlJwXZYeCxbJD5E1+e2goFcRZ07cI/FTu";` }}></script>*/}
                     {/* <script type="text/javascript" src="/statics/js/jwplayer-cstm-btn.min.js" async></script> */}
-                    <script src="https://cdn.qgraph.io/dist/aiqua-wp.js"></script>
+                    <script src="https://cdn.qgraph.io/dist/aiqua-wp.js" defer></script>
                     <script dangerouslySetInnerHTML={{ __html: `
                         AIQUA.init({
                             appId: 'c63c2960bf562e9ec2de',
@@ -173,7 +173,7 @@ class Default_v2 extends React.Component {
                 ) : <div></div>}
 
                 <div style={{ overflowX: 'hidden', height: '100%', marginTop: 0, paddingBottom: (this.platform && (this.platform == 'android' || this.platform == 'ios')) ? '0 !important' : '' }} id="wr" className="wrapper has-text-centered">{this.props.children}</div>
-                {this.platform && (this.platform == 'android' || this.platform == 'ios') ? (<script src="/static/js/fontawesome.js" crossOrigin="anonymous"></script>) : (process.env.UI_VERSION == '2.0' ? (this.props.hideFooter || this.props.pages.hide_footer ? null : <Footer_v2 />) : (<Footer />))}
+                {this.platform && (this.platform == 'android' || this.platform == 'ios') ? (<script async src="/static/js/fontawesome.js" crossOrigin="anonymous"></script>) : (process.env.UI_VERSION == '2.0' ? (this.props.hideFooter || this.props.pages.hide_footer ? null : <Footer_v2 />) : (<Footer />))}
             </div>
         )
     }
