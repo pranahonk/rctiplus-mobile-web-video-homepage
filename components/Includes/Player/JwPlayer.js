7fc1a8458f36875b0d439f175815d0d0c1d4543d
anungbisa4 <anung.bisa4@gmail.com>
2020-06-09 17:16:38 +0700

    [change-player]init player component

---
+++ components/Includes/Player/JwPlayer.js
@@ -0,0 +1,4 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { convivaVideoJs } from '../../../utils/conviva';
+import { getUserId } from '../../../utils/appier';
+import { onTrackingClick } from '../program-detail/programDetail';
