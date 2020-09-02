3b6f16cca06d587b0125fe9151832bbb8ce6b07c
anungbisa4 <anung.bisa4@gmail.com>
2020-09-02 11:12:13 +0700

    fix folder icons

--- components/Includes/Common/GridMenu.js
+++ components/Includes/Common/GridMenu.js
@@ -27,7 +27,7 @@ class GridMenu extends React.Component {
                             Router.push('/exclusive');
                         }}>
                             <a>
-                                <img className="menu-icon" src={'/icons/exclusive.svg'}/>
+                                <img className="menu-icon" src={'/icons-menu/exclusive.svg'}/>
                                 <p className="menu-label">Exclusive</p>
                             </a>
                         </div>
@@ -38,7 +38,7 @@ class GridMenu extends React.Component {
                                 <div onClick={() => {
                                     homeGeneralClicked('mweb_drama_clicked');
                                 }}>
-                                    <img className="menu-icon" src={'/icons/drama.svg'} style={{ width: 35 }}/>
+                                    <img className="menu-icon" src={'/icons-menu/drama.svg'} style={{ width: 35 }}/>
                                     <p className="menu-label">Drama</p>
                                 </div>
                             </a>
@@ -50,7 +50,7 @@ class GridMenu extends React.Component {
                                 <div onClick={() => {
                                     homeGeneralClicked('mweb_comedy_clicked');
                                 }}>
-                                    <img className="menu-icon" src={'/icons/comedy.svg'}/>
+                                    <img className="menu-icon" src={'/icons-menu/comedy.svg'}/>
                                     <p className="menu-label">Comedy</p>
                                 </div>
                             </a>
