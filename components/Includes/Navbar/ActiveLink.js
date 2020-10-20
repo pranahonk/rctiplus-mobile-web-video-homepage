165d971da25c550b442232bff49ab37b5ef2d68f
anungbisa4 <anung.bisa4@gmail.com>
2020-10-20 09:22:12 +0700

    add paid video swipe up

--- components/Includes/Navbar/ActiveLink.js
+++ components/Includes/Navbar/ActiveLink.js
@@ -7,6 +7,7 @@ const ActiveLink = ({ children, activeClassName, ...props }) => {
   const { asPath } = useRouter();
   const child = Children.only(children);
   const childClassName = child.props.className || '';
+  // console.log(props)
   const className =
     asPath === props.href  ||
     props.activeMenu === 'home/explores' || props.activeMenu.includes('home/explores') ||
