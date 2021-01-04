17ca42f6c64e07e9a8d9b2701a907be0aed0ff09
anungbisa4 <anung.bisa4@gmail.com>
2021-01-04 11:42:11 +0700

    news v2 style and interest

--- components/Includes/news/ThumbnailNews.js
+++ components/Includes/news/ThumbnailNews.js
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react'
+import Img from 'react-image';
 
 const ThumbnailNews = (props) => {
   return(
     <>
-      <div>Test</div>
+      <Img
+        alt={'null'} 
+        unloader={<img src="/static/placeholders/placeholder_landscape.png"/>}
+        loader={<img src="/static/placeholders/placeholder_landscape.png"/>}
+        src={['/static/placeholders/placeholder_landscape.png', '/static/placeholders/placeholder_landscape.png']} />
     </>
   )
 }
 
-export default ThumbnailNews
+export default ThumbnailNews;
