44619aa0c49718bb5cc1b320b3df1e89e52e41f2
Ikhsan Nurrahim <ikhsan.nurrahim@gmail.com>
2021-02-02 17:59:14 +0700

    add ads targeting value from api

--- components/Includes/Player/JwPlayer.js
+++ components/Includes/Player/JwPlayer.js
@@ -531,13 +531,22 @@ const JwPlayer = (props) => {
 
             googletag.defineSlot(slotName, [[maxWidth, maxHeight], [minWidth, minHeight]], slotDiv)
             .defineSizeMapping(mappingSlot)
-            .addService(googletag.pubads())
-            .setTargeting('logged_in', props.customData && props.customData.isLogin.toString())
+            .addService(googletag.pubads());
+            /* .setTargeting('logged_in', props.customData && props.customData.isLogin.toString())
             .setTargeting('channel_id', props.data && props.data.id)
             .setTargeting('program_title', props.type === 'live tv' ?
             tempId(props.data && props.data.id)[1] :
             props.type === 'live event' || props.type === 'missed event' ?
-            props.data && props.data.assets_name : 'NOT_SET');
+            props.data && props.data.assets_name : 'NOT_SET'); */
+
+            // TODO: looping targeting value
+            if (custParams != null) {
+              for (const custParam of custParams) {
+                console.log(custParam.name, custParam.value);
+                googletag.pubads().setTargeting(custParam.name, custParam.value);
+              }
+            }
+
             googletag.pubads().enableSingleRequest();
             googletag.pubads().collapseEmptyDivs();
             googletag.pubads().disableInitialLoad();
@@ -571,7 +580,7 @@ const JwPlayer = (props) => {
 
               adsImage.style.width = minWidth + 'px';
               adsImage.style.height = minHeight + 'px'; */
-              
+
               if (prevWidth != windowWidth) {
                 setPrevWidth(windowWidth);
                 googletag.pubads().refresh();
