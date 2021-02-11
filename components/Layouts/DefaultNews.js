dd7ec148bd4fab63dc649ff8544e26b08e2792a6
anungbisa4 <anung.bisa4@gmail.com>
2021-02-11 13:48:30 +0700

    change google analytic

--- components/Layouts/DefaultNews.js
+++ components/Layouts/DefaultNews.js
@@ -12,7 +12,7 @@ import chatsActions from '../../redux/actions/chats';
 import Footer from '../../components/Includes/Footer/Default';
 import Footer_v2 from '../../components/Includes/Footer/Default_v2';
 
-import { AUTHOR, VIEWPORT, MAIN_DESCRIPTION, OPEN_GRAPH } from '../../config';
+import { AUTHOR, VIEWPORT, MAIN_DESCRIPTION, OPEN_GRAPH, GTM } from '../../config';
 
 import queryString from 'query-string';
 import { isIOS, isAndroid } from "react-device-detect";
@@ -108,10 +108,20 @@ class DefaultNews extends React.Component {
                         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=ogx0OihVjJdMhvsmTnEFyA&gtm_preview=env-41&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=ogx0OihVjJdMhvsmTnEFyA&gtm_preview=${GTM}&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
                         })(window,document,'script','dataLayer','GTM-WJNRTJP');
                     ` }}></script>
                     {/* End Google Tag Manager */}
+                    <script dangerouslySetInnerHTML={{ __html: `
+                        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+                        ga('create', 'UA-145455301-17', 'auto', 'teamTracker');
+
+                        ga('teamTracker.send', 'pageview');
+                    ` }}></script>
 
                     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                     <script dangerouslySetInnerHTML={{ __html: `
@@ -143,7 +153,7 @@ class DefaultNews extends React.Component {
                     {/* End Comscore */}
 
                     {/* <!-- Google Tag Manager (noscript) --> */}
-                    <noscript key="gtm-noscript"><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WJNRTJP&gtm_auth=ogx0OihVjJdMhvsmTnEFyA&gtm_preview=env-41&gtm_cookies_win=x"
+                    <noscript key="gtm-noscript"><iframe src={`https://www.googletagmanager.com/ns.html?id=GTM-WJNRTJP&gtm_auth=ogx0OihVjJdMhvsmTnEFyA&gtm_preview=${GTM}&gtm_cookies_win=x`}
                     height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
                     {/* <!-- End Google Tag Manager (noscript) --> */}
 
