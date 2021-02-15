62167b59aa0b8cede0a018dc5d541f317c4d8641
anungbisa4 <anung.bisa4@gmail.com>
2021-02-15 13:57:09 +0700

    update auth gtm

--- components/Layouts/DefaultNews.js
+++ components/Layouts/DefaultNews.js
@@ -108,7 +108,7 @@ class DefaultNews extends React.Component {
                         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=ogx0OihVjJdMhvsmTnEFyA&gtm_preview=${GTM}&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=bo364VFvv-awdeFc2S8KmA&gtm_preview=${GTM}&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
                         })(window,document,'script','dataLayer','GTM-WJNRTJP');
                     ` }}></script>
                     {/* End Google Tag Manager */}
@@ -153,7 +153,7 @@ class DefaultNews extends React.Component {
                     {/* End Comscore */}
 
                     {/* <!-- Google Tag Manager (noscript) --> */}
-                    <noscript key="gtm-noscript"><iframe src={`https://www.googletagmanager.com/ns.html?id=GTM-WJNRTJP&gtm_auth=ogx0OihVjJdMhvsmTnEFyA&gtm_preview=${GTM}&gtm_cookies_win=x`}
+                    <noscript key="gtm-noscript"><iframe src={`https://www.googletagmanager.com/ns.html?id=GTM-WJNRTJP&gtm_auth=bo364VFvv-awdeFc2S8KmA&gtm_preview=${GTM}&gtm_cookies_win=x`}
                     height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
                     {/* <!-- End Google Tag Manager (noscript) --> */}
 
