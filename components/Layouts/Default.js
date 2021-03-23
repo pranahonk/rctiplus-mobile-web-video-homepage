ac870c0d55414f746d678a212e6c600e911ff3db
Setiady Mulya <setiady.mulya@mncgroup.com>
2021-03-23 16:12:36 +0700

    fix gpt

--- components/Layouts/Default.js
+++ components/Layouts/Default.js
@@ -86,6 +86,7 @@ class Default extends React.Component {
                         window.dataLayer.push({
                             'pilar' : 'news'
                         });
+
                         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
                         var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;
                         j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);}
