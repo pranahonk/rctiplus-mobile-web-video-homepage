7198b8e8e64829a7810abbcf4aee674732876c70
Setiady Mulya <setiady.mulya@mncgroup.com>
2021-03-23 15:39:09 +0700

    data layer server dev

--- components/Layouts/Default_v2.js
+++ components/Layouts/Default_v2.js
@@ -125,7 +125,6 @@ class Default_v2 extends React.Component {
                         window.dataLayer.push({
                             'pilar' : 'news'
                         });
-
                         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
