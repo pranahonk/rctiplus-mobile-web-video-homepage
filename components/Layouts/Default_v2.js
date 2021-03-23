4e25121cb092369c39db464a5afc64ce4ad9f131
Setiady Mulya <setiady.mulya@mncgroup.com>
2021-03-23 15:11:40 +0700

    add data layer pillar news

--- components/Layouts/Default_v2.js
+++ components/Layouts/Default_v2.js
@@ -115,6 +115,10 @@ class Default_v2 extends React.Component {
                     {/* <script data-ad-client="ca-pub-7595624984434758" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> */}
                     {/* Google Tag Manager */}
                     <script dangerouslySetInnerHTML={{ __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        window.dataLayer.push({
+                            'pilar' : 'news'
+                        });
                         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
