7ee7d65b79564f0c8b91618bb7c77767cffc8fe7
anungbisa4 <anung.bisa4@gmail.com>
2021-04-07 12:21:31 +0700

    appier - fix integrate appier

--- components/Layouts/DefaultNews.js
+++ components/Layouts/DefaultNews.js
@@ -12,7 +12,7 @@ import chatsActions from '../../redux/actions/chats';
 import Footer from '../../components/Includes/Footer/Default';
 import Footer_v2 from '../../components/Includes/Footer/Default_v2';
 
-import { AUTHOR, VIEWPORT, MAIN_DESCRIPTION, OPEN_GRAPH, GTM, GTM_AUTH, SHARE_BASE_URL } from '../../config';
+import { AUTHOR, VIEWPORT, MAIN_DESCRIPTION, OPEN_GRAPH, GTM, GTM_AUTH, SHARE_BASE_URL, MODE, APPIER_ID } from '../../config';
 
 import queryString from 'query-string';
 import { isIOS, isAndroid } from "react-device-detect";
@@ -146,7 +146,7 @@ class DefaultNews extends React.Component {
                     <script src="https://cdn.qgraph.io/dist/aiqua-wp.js" ></script>
                     <script dangerouslySetInnerHTML={{ __html: `
                         AIQUA.init({
-                            appId: 'c63c2960bf562e9ec2de',
+                            appId: '${APPIER_ID}',
                             timeout: 5000
                         });
                     `}}>
@@ -175,7 +175,8 @@ class DefaultNews extends React.Component {
                 </Head>
 
                 {/* <!-- DO NOT touch the following DIV --> */}
-                <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
+                {MODE === 'PRODUCTION' ? (
+                    <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
                     !function(q,g,r,a,p,h,js) {
                         if(q.qg)return;
                         js = q.qg = function() {
@@ -186,6 +187,19 @@ class DefaultNews extends React.Component {
                         h.parentNode.insertBefore(p,h);
                     } (window,document,'script','//cdn.qgr.ph/qgraph.c63c2960bf562e9ec2de.js');
                 ` }}></script>
+                ) : (
+                <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
+                    !function(q,g,r,a,p,h,js) {
+                        if(q.qg)return;
+                        js = q.qg = function() {
+                        js.callmethod ? js.callmethod.call(js, arguments) : js.queue.push(arguments);
+                        };
+                        js.queue = [];
+                        p=g.createElement(r);p.async=!0;p.src=a;h=g.getElementsByTagName(r)[0];
+                        h.parentNode.insertBefore(p,h);
+                    } (window,document,'script','https://cdn.qgr.ph/qgraph.3be8515a1c2359442dfd.js');
+                ` }}></script>
+                )}
 
 
                 <div style={{ overflowX: 'hidden', height: '100%', marginTop: 0, paddingBottom: (this.platform && (this.platform == 'android' || this.platform == 'ios')) ? '0 !important' : '' }} id="wr" className="wrapper has-text-centered">{this.props.children}</div>
