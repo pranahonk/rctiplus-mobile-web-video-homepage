480f3205daec3a861aebfe3c7aa527bba3c8d328
Setiady Mulya <setiady.mulya@mncgroup.com>
2021-04-12 16:52:47 +0700

    debug

--- pages/trending/detail_v2.js
+++ pages/trending/detail_v2.js
@@ -192,7 +192,7 @@ class Detail extends React.Component {
             .catch(error => {
                 console.log(error);
             });
-        alert('interest Topic >>' + window.location.href)
+        alert('detail >>' + window.location.href)
     }
 
     getRandom(arr, n) {
