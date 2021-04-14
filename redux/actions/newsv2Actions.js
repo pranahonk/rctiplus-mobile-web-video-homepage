3095e47a19f484799b16fb23277d35834e5dd458
anungbisa4 <anung.bisa4@gmail.com>
2021-04-14 09:53:36 +0700

    news - fix category id

--- redux/actions/newsv2Actions.js
+++ redux/actions/newsv2Actions.js
@@ -231,7 +231,7 @@ const getTrending = (category = 1, pageSize = 5, page = 1) => {
 const getSectionNews = (category_id = 16, pageSize = 3, page = 1) => {
     return dispatch => new Promise(async (resolve, reject) => {
         try {
-            const response = await axios.get(`/v2/section?category_id=${16}&page=${page}&pageSize=${pageSize}`);
+            const response = await axios.get(`/v2/section?category_id=${category_id}&page=${page}&pageSize=${pageSize}`);
             if (response.status === 200 && response.data.status.code === 0) {
                 dispatch({
                     type: 'GET_SECTION_NEWS',
