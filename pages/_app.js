d97dd972f798a7b5732b9d034fd437d6417596c4
Setiady Mulya <setiady.mulya@mncgroup.com>
2021-04-28 10:11:23 +0700

    detect query params platform

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,15 @@ import App from 'next/app';
 import withRedux from 'next-redux-wrapper';
 import { register, unregister } from 'next-offline/runtime';
 import initStore  from '../redux';
+
 import { setVisitorTokenNews, setNewsTokenV2, setNewsToken, setVisitorToken, getVisitorToken, getVisitorTokenNews, getNewsToken, getNewsTokenV2 } from '../utils/cookie';
 
 import 'sweetalert2/src/sweetalert2.scss';
 import '../assets/scss/apps/homepage/default.scss';
 
 import '../assets/scss/components/alert.scss';
+import queryString from 'query-string';
+import isEmpty from 'lodash/isEmpty'
 
 export default withRedux(initStore, { debug: false })(
     class MyApp extends App {
@@ -21,14 +24,33 @@ export default withRedux(initStore, { debug: false })(
         }
 
         async componentDidMount() {
-            if(screen.width < 500 ||
-                navigator.userAgent.match(/Android/i) ||
-                navigator.userAgent.match(/webOS/i) ||
-                navigator.userAgent.match(/iPhone/i) ||
-                navigator.userAgent.match(/iPod/i) || 
-                navigator.userAgent.match(/iPad/i)) {
-            } 
-            else {
+            const segments = this.props.router.asPath;
+            let params = {}
+            if (segments.length > 1) {
+                let qParams = segments.split(/\?/)
+                qParams.forEach((row) => {
+                    params = {...queryString.parse(row)}
+                 });
+            }
+            let condition = (
+                screen.width < 500 || (
+                    navigator.userAgent.match(/Android/i) ||
+                    navigator.userAgent.match(/webOS/i) ||
+                    navigator.userAgent.match(/iPhone/i) ||
+                    navigator.userAgent.match(/iPod/i) || 
+                    navigator.userAgent.match(/iPad/i)
+                ) || (
+                    !isEmpty(params.device) &&
+                    params.device === 'ipad'
+                ) || (
+                    !isEmpty(params.token)
+                ) || (
+                    !isEmpty(params.platform) &&
+                    params.platform !== 'null' &&
+                    params.platform === 'ios'
+                )
+            )
+            if(!condition) {
                 window.location.href = process.env.REDIRECT_WEB_DESKTOP + window.location.pathname;
             }
 
