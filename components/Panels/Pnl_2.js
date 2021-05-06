fd1c76db85fe6cad3d3137f5e650dab151c74b0b
anungbisa4 <anung.bisa4@gmail.com>
2021-05-06 22:57:47 +0700

    video - action click special banner

--- components/Panels/Pnl_2.js
+++ components/Panels/Pnl_2.js
@@ -40,7 +40,64 @@ class Pnl_2 extends React.Component {
 			homeGeneralClicked('mweb_homepage_scroll_horizontal');
 		}
 	}
-
+	handleActionClick(program, url) {
+			switch (program.action_type) {
+				case 'live_streaming' :
+					let channel = 'rcti'
+					if(program?.link === '1') {
+							channel = 'rcti'
+					}
+					if(program?.link === '2') {
+							channel = 'mnctv'
+					}
+					if(program?.link === '3') {
+							channel = 'gtv'
+					}
+					if(program?.link === '4') {
+							channel = 'inews'
+					}
+					Router.push(`/tv/${channel}`);
+				break;
+				case 'catchup':
+				if(program.link && program.channel && program.catchup_date) {
+						const title = program?.content_title?.replace(/[\/ !@#$%^&*(),.?":{}|<>-]/g, '-').replace(/(-+)/g, '-')
+						Router.push(`/tv/${program.channel}/${program.link}/${title}?date=${program.catchup_date}`)
+				}
+				break;
+				case 'scan_qr':
+					Router.push("/qrcode")
+				break;
+				case 'homepage_news':
+					Router.push("/news")
+				break;
+				case 'news_detail' :
+				case 'news_category':
+						window.open(program.link, '_parent');
+						break;
+				case 'episode':
+						if(program.link && program.program_id) {
+								const title = program.content_title.replace(/[\/ !@#$%^&*(),.?":{}|<>-]/g, '-').replace(/(-+)/g, '-')
+								Router.push(`/programs/${program.program_id}/${title}/episode/${program.link}/${title}`)
+						}
+						break;
+				case 'live_event':
+						if (program.link) {
+								Router.push(`/live-event/${program.link}/${program.content_title.replace(/[\/ !@#$%^&*(),.?":{}|<>-]/g, '-').replace(/(-+)/g, '-')}`);
+						}
+						break;
+				case 'genre':
+							Router.push(`/explores/${program.link}/${program.content_title.replace(/[\/ !@#$%^&*(),.?":{}|<>-]/g, '-').replace(/(-+)/g, '-')}`);
+						break;
+				case 'program':
+						Router.push(`/programs/${program.link}/${program.content_title.replace(/ +/g, '-')}`);
+						break;  
+				case 'popup':
+						window.open(url, '_parent');
+						break;  
+				default:
+						Router.push(`/tv/rcti`);
+		}       
+	}
 	link(data) {
 		// console.log('PANEL 2', data);
 		switch (data.content_type) {
@@ -66,8 +123,9 @@ class Pnl_2 extends React.Component {
 							<b>RCTI+</b>`, '', () => { }, true, 'Sign Up', 'Sign In', true, true);
 					}
 					else {
+						this.handleActionClick(data, url)
 						// window.open(url, '_blank');
-						window.location.href = url;
+						// window.location.href = url;
 					}
 				}
 				catch (e) {
