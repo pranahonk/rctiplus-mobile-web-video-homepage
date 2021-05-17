820829bf92f36e4ad5e628ca0be6d82005234875
anungbisa4 <anung.bisa4@gmail.com>
2021-05-17 15:01:22 +0700

    video - inprogress live tv v2

--- pages/tv_v2.js
+++ pages/tv_v2.js
@@ -3,16 +3,18 @@ import { useSelector, useDispatch } from "react-redux"
 
 import dynamic from "next/dynamic"
 import Layout from "../components/Layouts/Layout"
+import * as dayjs from "dayjs"
 
 import "../assets/scss/components/tv-v2.scss"
 
 const channel = ["RCTI", "MNCTV", "GTV", "iNEWS"]
+const weeks = ["SU", "MO", "TU", "WE", "TH", "FR", "SA"]
 
 
 function TV_V2 (props) {
   const [tabChannel, setTabChannel] = useState("RCTI")
 
-  console.log(tabChannel)
+  console.log(dayjs().day())
   return(
     <>
       <Layout>
@@ -29,7 +31,24 @@ function TV_V2 (props) {
               )
             })}
           </section>
-          <section>list week</section>
+          <section>
+            <div className="list-week_wrapper">
+              {weeks.map((week, index) => {
+                return(
+                  <div className="day-item_wrapper" key={index}>
+                    {index === dayjs().day() && (
+                      <>
+                        <label className="day-item_today">TODAY</label>
+                      </>
+                    )}
+                    <div className="day-item_text">{week}</div>
+                    <div className="day-item_date">{dayjs().day(index).format("DD")}</div>
+                    <span className="day-item_line active"/>
+                  </div>
+                )
+              })}
+            </div>
+          </section>
           <section>list item play</section>
         </div>
       </Layout>
