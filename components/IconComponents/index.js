c060e90091ef7621880bd3efe3f7e86b3a5bea3c
anungbisa4 <anung.bisa4@gmail.com>
2021-06-09 07:05:56 +0700

    bug - fix icon components

---
+++ components/IconComponents/index.js
@@ -0,0 +1,6 @@
+import CirclePauseIcon from "./CirclePauseIcon"
+import CircleTimeIcon from "./CircleTImeIcon"
+import CirclePlayIcon from "./CirclePlayIcon"
+import ShareIcon from "./ShareIcon"
+
+export { CirclePauseIcon, CircleTimeIcon, CirclePlayIcon, ShareIcon }
