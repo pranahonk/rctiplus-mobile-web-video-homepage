87037a1bf87c6fe47f3e2cb4bef966b8fd0d2613
Hasnawi Haeba <hasnawi@dalenta.tech>
2021-06-13 08:52:24 +0800

    update

--- components/Includes/Chat/LiveChat.js
+++ components/Includes/Chat/LiveChat.js
@@ -1,93 +1,204 @@
-import { useState} from "react"
+import { useState, useEffect, useContext } from "react"
+import { connect } from "react-redux"
 import { Row, Col, Button, Input } from 'reactstrap';
 import { Picker } from 'emoji-mart';
-import { ChevronDownIcon} from "../../../components/IconComponents"
+import { withRouter } from 'next/router';
+import Img from 'react-image';
+import TimeAgo from 'react-timeago';
+
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
 import SentimenVerySatifiedIcon from '@material-ui/icons/SentimentVerySatisfied';
 import SendIcon from '@material-ui/icons/Send';
 import KeyboardIcon from '@material-ui/icons/Keyboard';
+import { ChevronDownIcon} from "../../../components/IconComponents"
+
+import chatsActions from '../../../redux/actions/chats';
 
 import '../../../assets/scss/components/tv-v2.scss';
 import 'emoji-mart/css/emoji-mart.css';
+import ax from 'axios';
+
+import { initGA } from '../../../utils/firebaseTracking';
+import { getCookie, getVisitorToken, checkToken } from '../../../utils/cookie';
+import { DEV_API} from '../../../config';
+const axios = ax.create({baseURL: DEV_API + '/api'});
+
+axios.interceptors.request.use(async (request) => {
+  await checkToken();
+  const accessToken = getCookie('ACCESS_TOKEN');
+  request.headers['Authorization'] = accessToken == undefined ? getVisitorToken() : accessToken;
+  return request;
+});
 
 const Chat = ({...props}) => {
 	const inputChatBoxRef = React.createRef();
-	
+
 	const [chat, setChat] = useState("");
+	const [chats, setChats] = useState([]);
+	const [userData, setUserData] = useState(null);
   	const [emojiPickerOpen, setEmojiPickerOpen] = useState(false);
+	const [sendingChat, setSendingChat] = useState(false);
 
   	const handleToggelEmoji = () => setEmojiPickerOpen(!emojiPickerOpen);
 	const onSelectEmoji = (emoji) => setChat(chat+emoji.native);
+
+	const getUser = () =>{
+		axios.get('/v3/user')
+		.then(response => {
+		  if (response.data.data) setUserData(response.data.data)
+		})
+		.catch(error => console.log(error));
+	}
 	
-	const loadChatMessages = () =>{};
-	const sendChat = () => {};
+	const loadChatMessages = (id) => {
+		let firstLoadChat = true;
+			props.listenChatMessages(id)
+				.then(collection => {
+					console.log(`berhasil loadmessage`)
+					console.log(collection)
+					let snapshot = collection.orderBy('ts', 'desc').limit(10).onSnapshot(querySnapshot => {
+					querySnapshot.docChanges()
+						.map(change => {
+							let chatsTemp = chats
+							if (change.type === 'added') {
+								if (!sendingChat) {
+									console.log(`inimi haha`,change.doc.data())
+										if (chatsTemp.length > 0) {
+											let lastChat = chatsTemp
+											let newChat = change.doc.data();
+											if ((lastChat && newChat) && (lastChat.u != newChat.u || lastChat.m != newChat.m || lastChat.i != newChat.i)) {
+												if (firstLoadChat) chatsTemp.unshift(newChat);
+												else {
+													chatsTemp.push(newChat);
+													// this.state.total_newChat.push(newChat)
+												}
+											}
+										}
+										else {
+											if (firstLoadChat) chatsTemp.unshift(change.doc.data());	
+											else {
+												chatsTemp.push(change.doc.data());
+												// this.state.total_newChat.push(change.doc.data())
+											}
+										}
+									console.log(`ini chat temp`, chatsTemp)
+									setChats(chatsTemp);
+								}
+							}
+						});
 
-  return(
-    <div className="box-chat" >
-      <div onClick={props.toggle} className="chat-wrapper" style={{borderRadius:"0px", padding: "15px"}}>
-        <ChevronDownIcon />
-          <label className="chat-text">
-            Live Chat
-            <span className="circle-chat" style={{background:"#6DD400"}} />
-          </label>
-      </div>
+						firstLoadChat = false;
+					});
+				});
+	};
 
-      <div  className="chat-messages" id="chat-messages">
-        <Row  className="chat-line">
-			<Col xs={2}> 
-            	Img  
-				{/* <Img
-					loader={<PersonOutlineIcon className="chat-avatar" />}
-					unloader={<PersonOutlineIcon className="chat-avatar" />}
-					className="chat-avatar" src={[chat.i, '/static/icons/person-outline.png']} /> */}
-			</Col>
-			<Col className="chat-message" xs={10}> 
-            This chat 
-				{/* {chat.sent != undefined && chat.failed != undefined ? (chat.sent == true && chat.failed == true ? (<span onClick={() => this.resendChat(i)}><RefreshIcon className="message" /> <small style={{ marginRight: 10, fontSize: 8, color: 'red' }}>failed</small></span>) : (<TimeAgo className="timeago" minPeriod={60} date={Date.now() - (Date.now() - chat.ts)} />)) : (<TimeAgo className="timeago" minPeriod={60} date={Date.now() - (Date.now() - chat.ts)} />)} <span className="username">{chat.u}</span> <span className="message">{chat.m}</span> */}
-			</Col>
-		</Row>
-      </div>
+	const sendChat = (id) => {
+		if(userData){
+			if(chat !== ""){
+				const userCurrent = userData
+				let user = userCurrent.nickname ? userCurrent.nickname :
+				userCurrent.display_name ? userCurrent.display_name :
+				userCurrent.email ? userCurrent.email.replace(/\d{4}$/, '****') :
+					userCurrent.phone_number ? userCurrent.phone_number.substring(0, userCurrent.phone_number.lastIndexOf("@")) : 'anonymous';
+				let newChat = {
+					ts: Date.now(),
+					m: chat,
+					u: user,
+					i: userData.photo_url,
+					sent: false,
+					failed: false
+				};
+				let chatsCurrent = chats;
+				chatsCurrent.push(newChat);
+			}
+		}
+	};
 
-      <div className="chat-input-box">
-			<div ref={inputChatBoxRef} className="chat-box">
-				<Row>
-					<Col xs={1}>
-						<Button className="emoji-button">
-							{emojiPickerOpen ? (<KeyboardIcon onClick={handleToggelEmoji} />) : (<SentimenVerySatifiedIcon onClick={handleToggelEmoji} />)}
-						</Button>
-					</Col>
-					<Col xs={9}>
-						<Input
-							// onKeyDown={this.handleChatEnter.bind(this)}
-							onChange={e => setChat(e.target.value)}
-							// onClick={this.checkLogin.bind(this)}
-							value={chat}
-							type="textarea"
-							id="chat-input"
-							placeholder="Start Chatting"
-							className="chat-input"
-							maxLength={250}
-							rows={1} 
-						/>
-					</Col>
-					<Col xs={1}>
-						<Button className="send-button" >
-							<SendIcon />
-						</Button>
-					</Col>
-				</Row>
+	useEffect(() =>{
+		initGA();
+		getUser();
+	}, [])
+
+	useEffect(() => {
+		loadChatMessages(props.id)
+	}, [props.id])
+
+	console.log(`ini chats`, chats)
+
+  	return(
+		<>
+			<div onClick={props.toggle} className="chat-wrapper" style={{borderRadius:"0px", padding: "15px"}}>
+				<ChevronDownIcon />
+				<label className="chat-text">
+					Live Chat
+					<span className="circle-chat" style={{background:"#6DD400"}} />
+				</label>
 			</div>
 
-			<Picker
-				onSelect={emoji => {
-					onSelectEmoji(emoji);
-				}}
-				showPreview={false}
-				darkMode
-				style={{ display: emojiPickerOpen ? 'block' : 'none' }} 
-			/>			
-		</div>
-    </div>
-  )
+			
+				<div  className="box-chat" >
+				<div className="chat-messages" >
+					{chats.map((val, i) => (
+						<Row  className="chat-line">
+							<Col xs={2}>   
+								<Img
+									loader={<PersonOutlineIcon className="chat-avatar" />}
+									unloader={<PersonOutlineIcon className="chat-avatar" />}
+									className="chat-avatar" src={[val.i, '/static/icons/person-outline.png']} />
+							</Col>
+							<Col className="chat-message" xs={10}> 
+								{val?.sent != undefined && val?.failed != undefined ? (val?.sent == true && val?.failed == true ? (<span><RefreshIcon className="message" /> <small style={{ marginRight: 10, fontSize: 8, color: 'red' }}>failed</small></span>) : (<TimeAgo className="timeago" minPeriod={60} date={Date.now() - (Date.now() - val?.ts)} />)) : (<TimeAgo className="timeago" minPeriod={60} date={Date.now() - (Date.now() - val?.ts)} />)} 
+								<span className="username">{val?.u}</span> <span className="message">{val?.m}</span>
+							</Col>
+						</Row>
+					))}
+				</div>
+
+				<div className="chat-input-box">
+					<div ref={inputChatBoxRef} className="chat-box">
+						<Row>
+							<Col xs={1}>
+								<Button className="emoji-button">
+									{emojiPickerOpen ? (<KeyboardIcon onClick={handleToggelEmoji} />) : (<SentimenVerySatifiedIcon onClick={handleToggelEmoji} />)}
+								</Button>
+							</Col>
+							<Col xs={9}>
+								<Input
+									// onKeyDown={this.handleChatEnter.bind(this)}
+									onChange={e => setChat(e.target.value)}
+									// onClick={this.checkLogin.bind(this)}
+									value={chat}
+									type="textarea"
+									id="chat-input"
+									placeholder="Start Chatting"
+									className="chat-input"
+									maxLength={250}
+									rows={1} 
+								/>
+							</Col>
+							<Col xs={1}>
+								<Button className="send-button" >
+									<SendIcon />
+								</Button>
+							</Col>
+						</Row>
+					</div>
+
+					<Picker
+						onSelect={emoji => {
+							onSelectEmoji(emoji);
+						}}
+						showPreview={false}
+						darkMode
+						style={{ display: emojiPickerOpen ? 'block' : 'none' }} 
+					/>			
+				</div>
+			</div>
+			
+		</>
+  	)
 }
 
-export default Chat
+export default connect(state => state, {
+	...chatsActions,
+})(withRouter(Chat));
---
+++ components/Includes/Chat/LiveChat_v2.js
@@ -0,0 +1,252 @@
+import React, { Fragment } from "react";
+import { connect } from 'react-redux';
+import { withRouter } from 'next/router';
+import nextCookie from 'next-cookies'
+import { Row, Col, Button, Input } from 'reactstrap';
+import { Picker } from 'emoji-mart';
+import Img from 'react-image';
+import TimeAgo from 'react-timeago';
+
+import { ChevronDownIcon} from "../../../components/IconComponents"
+import SentimenVerySatifiedIcon from '@material-ui/icons/SentimentVerySatisfied';
+import SendIcon from '@material-ui/icons/Send';
+import KeyboardIcon from '@material-ui/icons/Keyboard';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+
+import liveAndChatActions from '../../../redux/actions/liveAndChatActions';
+import pageActions from '../../../redux/actions/pageActions';
+import chatsActions from '../../../redux/actions/chats';
+import userActions from '../../../redux/actions/userActions';
+
+import '../../../assets/scss/components/tv-v2.scss';
+import 'emoji-mart/css/emoji-mart.css';
+
+class Chat extends React.Component {
+
+    constructor(props) {
+	    super(props);
+        this.inputChatBoxRef = React.createRef();
+        this.state={
+            emojiPickerOpen: false, 
+            chat: "",
+            user_data: null,
+			sending_chat: false,
+			chats: [],
+			total_newChat: []
+        }
+	}
+
+    componentDidMount() {
+		this.loadChatMessages(this.props.id)
+        this.props.getUserData()
+			.then(response => {
+				console.log(response);
+				if (response.status === 200 && response.data.status.code === 0) {
+					this.setState({ user_data: response.data.data });
+				}
+			})
+			.catch(error => {
+				console.log(error);
+		});
+    }
+
+    handleToggelEmoji(){
+        this.setState({emojiPickerOpen: !this.state.emojiPickerOpen})
+    }
+
+    onSelectEmoji(emoji) {
+		this.setState({ chat: this.state.chat + emoji.native });
+	}
+
+    loadChatMessages(id) {
+		this.setState({ chats: [] }, () => {
+			const chatBox = document.getElementById('chat-messages');
+			chatBox.scrollTop = chatBox.scrollHeight;
+				let firstLoadChat = true;
+				this.props.listenChatMessages(id)
+					.then(collection => {
+						let snapshots = this.state.snapshots;
+						let snapshot = collection.orderBy('ts', 'desc').limit(10).onSnapshot(querySnapshot => {
+							querySnapshot.docChanges()
+								.map(change => {
+									let chats = this.state.chats;
+									console.log(chats);
+									if (change.type === 'added') {
+										if (!this.state.sending_chat) {
+											if (chats.length > 0) {
+												let lastChat = chats
+												let newChat = change.doc.data();
+												
+												if ((lastChat && newChat) && (lastChat.u != newChat.u || lastChat.m != newChat.m || lastChat.i != newChat.i)) {
+													if (firstLoadChat) {
+														chats.unshift(newChat);
+													}
+													else {
+														chats.push(newChat);
+														this.state.total_newChat.push(newChat)
+													}
+												}
+											}
+											else {
+												if (firstLoadChat) {
+													chats.unshift(change.doc.data());	
+												}
+												else {
+													chats.push(change.doc.data());
+													this.state.total_newChat.push(change.doc.data())
+												}
+											}
+											
+											if(!this.state.chat_box && this.state.total_newChat.length > 0){
+												const chatBox = document.getElementById('chat-messages');
+												chatBox.scrollTop = chatBox.scrollHeight;
+											}
+
+											this.setState({ chats: chats }, () => {
+												const chatInput = document.getElementById('chat-input');
+												chatInput.style.height = `24px`;
+											});
+										}
+									}
+								});
+
+							firstLoadChat = false;
+						});
+					});
+		});
+	}
+
+    sendChat() {
+		if (this.state.user_data) {
+			if (this.state.chat != '') {
+				// this.statusChatBlock(this.state.live_events[this.state.selected_index].id ? this.state.live_events[this.state.selected_index].id : this.state.live_events[this.state.selected_index].content_id);
+				const userData = this.state.user_data;
+				let user = userData.nickname ? userData.nickname :
+					userData.display_name ? userData.display_name :
+						userData.email ? userData.email.replace(/\d{4}$/, '****') :
+							userData.phone_number ? userData.phone_number.substring(0, userData.phone_number.lastIndexOf("@")) : 'anonymous';
+				let newChat = {
+					ts: Date.now(),
+					m: this.state.chat,
+					u: user,
+					i: this.state.user_data.photo_url,
+					sent: false,
+					failed: false
+				};
+				let chats = this.state.chats;
+				chats.push(newChat);
+
+				this.setState({ chats: chats, chat: '', sending_chat: true }, () => {
+					const chatBox = document.getElementById('chat-messages');
+					chatBox.scrollTop = chatBox.scrollHeight;
+
+					const chatInput = document.getElementById('chat-input');
+					chatInput.style.height = `24px`;
+
+					this.props.setChat(this.props.id, newChat.m, user, this.state.user_data.photo_url)
+						.then(response => {
+							newChat.sent = true;
+							if (response.status !== 200 || response.data.status.code !== 0) {
+								newChat.failed = true;
+							}
+							chats[chats.length - 1] = newChat;
+							this.setState({ chats: chats, sending_chat: false });
+						})
+						.catch(() => {
+							newChat.sent = true;
+							newChat.failed = true;
+							chats[chats.length - 1] = newChat;
+							this.setState({ chats: chats, sending_chat: false });
+						});
+				});
+			}
+		}
+		else {
+			// showSignInAlert(`Please <b>Sign In</b><br/>
+			// Woops! Gonna sign in first!<br/>
+			// Only a click away and you<br/>
+			// can continue to enjoy<br/>
+			// <b>RCTI+</b>`, '', () => { }, true, 'Sign Up', 'Sign In', true, true);
+		}
+	}
+
+    render(){
+        return(
+			<Fragment>
+				<div onClick={this.props.toggle} className="chat-wrapper" style={{borderRadius:"0px", padding: "15px"}}>
+                    <ChevronDownIcon />
+                    <label className="chat-text">
+                        Live Chat
+                        <span className="circle-chat" style={{background:"#6DD400"}} />
+                    </label>
+                </div>
+				
+				<div className="box-chat" >
+					<div  className="chat-messages" id="chat-messages">
+						{this.state.chats.map((chat, i) => (
+								<Row key={i} className="chat-line">
+									<Col xs={2}>
+										<Img
+											loader={<PersonOutlineIcon className="chat-avatar" />}
+											unloader={<PersonOutlineIcon className="chat-avatar" />}
+											className="chat-avatar" src={[chat.i, '/static/icons/person-outline.png']} />
+									</Col>
+									<Col className="chat-message" xs={10}>
+										{chat.sent != undefined && chat.failed != undefined ? (chat.sent == true && chat.failed == true ? (<span onClick={() => this.resendChat(i)}><RefreshIcon className="message" /> <small style={{ marginRight: 10, fontSize: 8, color: 'red' }}>failed</small></span>) : (<TimeAgo className="timeago" minPeriod={60} date={Date.now() - (Date.now() - chat.ts)} />)) : (<TimeAgo className="timeago" minPeriod={60} date={Date.now() - (Date.now() - chat.ts)} />)} <span className="username">{chat.u}</span> <span className="message">{chat.m}</span>
+									</Col>
+								</Row>
+						))}
+					</div>
+
+                	<div className="chat-input-box">
+                        <div ref={this.inputChatBoxRef} className="chat-box">
+                            <Row>
+                                <Col xs={1}>
+                                    <Button className="emoji-button">
+                                        {this.state.emojiPickerOpen ? (<KeyboardIcon onClick={this.handleToggelEmoji.bind(this)} />) : (<SentimenVerySatifiedIcon onClick={this.handleToggelEmoji.bind(this)} />)}
+                                    </Button>
+                                </Col>
+                                <Col xs={9}>
+                                    <Input
+                                        // onKeyDown={this.handleChatEnter.bind(this)}
+                                        onChange={e => this.setState({chat: e.target.value})}
+                                        // onClick={this.checkLogin.bind(this)}
+                                        value={this.state.chat}
+                                        type="textarea"
+                                        id="chat-input"
+                                        placeholder="Start Chatting"
+                                        className="chat-input"
+                                        maxLength={250}
+                                        rows={1} 
+                                    />
+                                </Col>
+                                <Col xs={1}>
+                                    <Button className="send-button" onClick={this.sendChat.bind(this)} >
+                                        <SendIcon />
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </div>
+
+                        <Picker
+                            onSelect={emoji => {
+                                this.onSelectEmoji(emoji);
+                            }}
+                            showPreview={false}
+                            darkMode
+                            style={{ display: this.state.emojiPickerOpen ? 'block' : 'none' }} 
+                        />			
+                    </div>
+            </div>
+			</Fragment>
+        )
+    }
+
+}
+
+export default connect(state => state, {
+	...liveAndChatActions,
+	...pageActions,
+	...chatsActions,
+	...userActions
+})(withRouter(Chat));
