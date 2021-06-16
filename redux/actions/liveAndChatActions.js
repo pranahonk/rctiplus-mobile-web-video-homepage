48fde600dc70a287d2f8a73583d142845b5ae401
anungbisa4 <anung.bisa4@gmail.com>
2021-06-16 15:59:35 +0700

    fix - ta on catchup tv

--- redux/actions/liveAndChatActions.js
+++ redux/actions/liveAndChatActions.js
@@ -313,7 +313,7 @@ const getEPG = (date, channel = 'rcti') => {
 const getCatchupUrl = catchupId => {
     return dispatch => new Promise(async (resolve, reject) => {
         try {
-            const response = await axios.get(`/v1/epg/${catchupId}`);
+            const response = await axios.get(`/v1/epg/${catchupId}?appierid=${getUidAppier()}`);
             if (response.data.status.code === 0) {
                 dispatch({
                     type: 'GET_CATCHUP_URL',
