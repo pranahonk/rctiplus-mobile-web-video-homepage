bb5531f23196b2e038ac2c2327b2f6bc3276048a
anungbisa4 <anung.bisa4@gmail.com>
2021-07-14 15:16:38 +0700

    fix cookie logout

--- utils/cookie.js
+++ utils/cookie.js
@@ -41,13 +41,37 @@ export const setCookie = (name, value, days) => {
          domain = '.'+domainParts.join('.');
 
          document.cookie = name+"="+value+expires+"; path=/; domain="+domain;
-        //  if (Cookie.get(name) == null || Cookie.get(name) != value)
-        //  {
+         if (getCookieHelper(name) == null || getCookieHelper(name) != value)
+         {
 
-        //     domain = '.'+host;
-        //     document.cookie = name+"="+value+expires+"; path=/; domain="+domain;
-        //  }
+            domain = '.'+host;
+            document.cookie = name+"="+value+expires+"; path=/; domain="+domain;
+         }
       }
+};
+
+const getCookieHelper = (name) => {
+    {
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(';');
+      for (var i=0; i < ca.length; i++)
+      {
+         var c = ca[i];
+         while (c.charAt(0)==' ')
+         {
+            c = c.substring(1,c.length);
+         }
+
+         if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+      }
+      return null;
+   }
+}
+
+export const erese = key => {
+    if (process.browser) {
+        setCookie(key, '', -1)
+    }
 };
 
 export const removeCookie = key => {
