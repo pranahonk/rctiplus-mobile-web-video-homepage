b20a3d6402cce99156589f3a43e1f47863bb7feb
anungbisa4 <anung.bisa4@gmail.com>
2021-07-29 10:35:33 +0700

    fix close stories

--- components/Includes/Gallery/Stories_v2.js
+++ components/Includes/Gallery/Stories_v2.js
@@ -56,7 +56,7 @@ class Stories extends React.Component {
                     }, () => {
                         const currentSkin = this.getCurrentSkin();
                         this.storiesApi = new this.state.zuckJS("stories-react", {
-                            backButton: true,
+                            // backButton: true,
                             backNative: false,
                             previousTap: true,
                             skin: currentSkin['name'],
@@ -182,7 +182,7 @@ class Stories extends React.Component {
                     }, () => {
                         const currentSkin = this.getCurrentSkin();
                         this.storiesApi = new this.state.zuckJS("stories-react", {
-                            backButton: true,
+                            // backButton: true,
                             backNative: false,
                             previousTap: true,
                             skin: currentSkin['name'],
@@ -385,7 +385,7 @@ class Stories extends React.Component {
             items.push([
                 item.id,
                 item.link_video != null ? 'video' : 'photo',
-                10,
+                100000,
                 item.link_video != null ? (item.link_video) : (this.props.stories.meta.image_path + this.state.resolution + item.story_img),
                 item.link_video != null ? (item.link_video) : (this.props.stories.meta.image_path + this.state.resolution + item.story_img),
                 this.handleActionClick(item), 
