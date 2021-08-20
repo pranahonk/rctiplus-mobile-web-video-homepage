751752bbadcd9640b2771a00044bfc616c2b75b1
andrumahardi <andrumahardi77@gmail.com>
2021-08-20 22:32:39 +0700

    feature - library revamp minified banner vison+

--- pages/explores_revamp.js
+++ pages/explores_revamp.js
@@ -171,7 +171,7 @@ class ExploresRevamp extends React.Component {
         }
 
 				<div id="library-revamp">
-					<figure style={{width: "100%", margin: "0 0 1rem 0"}}>
+					<figure style={{width: "95%", margin: "0 0 1rem 0"}}>
 						<img src="static/img/homepage_revamp_library.svg" width="100%" />
 					</figure>
 					<button onClick={_ => this.redirectToVideoPlus()}>
