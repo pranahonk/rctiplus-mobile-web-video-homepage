f601dbde851b89c8ed76df8ae0aa9dbb5acbc1ba
andrumahardi <andru.mahardi@mncgroup.com>
2021-11-10 15:42:41 +0700

    lineup homepage first init

--- graphql/queries/homepage.js
+++ graphql/queries/homepage.js
@@ -11,4 +11,62 @@ export const GET_BANNERS = gql`
       }
     }
   }
-`;
+`
+
+export const GET_LINEUPS = (page = 1, pageSize = 10) => {
+  return gql`
+    query {
+      lineups(page: ${page}, page_size: ${pageSize}) {
+        data {
+          id
+          lineup_type
+          content_type
+          service
+          title
+          display_type
+          sorting
+          lineup_type_detail {
+            ... on LineupTypeDefault {
+              detail {
+                data {
+                  content_id
+                  content_type
+                  content_type_detail {
+                    ... on ContentTypeEpisode {
+                      detail {
+                        data {
+                          portrait_image
+                        }
+                      }
+                    }
+                    ... on ContentTypeProgram {
+                      detail {
+                        data {
+                          portrait_image
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            ... on LineupTypeStory {
+              detail {
+                data {
+                  program_id
+                }
+              }
+            }
+          }
+        }
+        meta {
+          pagination {
+            current_page
+            total_page
+          }
+          image_path
+        }
+      }
+    }
+  `
+}
