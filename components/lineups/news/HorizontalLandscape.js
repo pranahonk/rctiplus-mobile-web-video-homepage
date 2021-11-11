a1d5d5e0877038e974f04cd4420a4a6341ac4335
prana.wijaya <1911510996@student.budiluhur.ac.id>
2021-11-11 15:02:23 +0700

    fixing meta not found

--- components/lineups/news/HorizontalLandscape.js
+++ components/lineups/news/HorizontalLandscape.js
@@ -54,14 +54,14 @@ const HorizontalLandscape = ({...props}) => {
   },[list]);
   useEffect(() => {
     setAssetUrl(list?.meta && list.meta.assets_url ? list.meta.assets_url : null);
-    if (list.data && (list?.meta?.pagination?.current_page < list?.meta?.pagination?.total_page) && show && list.data.length < 20) {
+    if (list.data && (list?.meta?.pagination?.current_page < list?.meta?.pagination?.total_page) && show && list?.data?.length < 20) {
       setLoadingMore(true);
     }
   }, [show, list])
 
   const _goToDetail = (article) => {
     let category = '';
-    if (article.subcategory_name.length < 1) {
+    if (article.subcategory_name?.length < 1) {
       category = 'berita-utama';
     } else {
       category = urlRegex(article.subcategory_name)
