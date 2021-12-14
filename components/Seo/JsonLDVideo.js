d7f9b6b9d228309fa3cc8a1ff2011defee73b6fb
Feri_Romadhona93 <feri.romadhona@mncgroup.com>
2021-12-14 17:26:50 +0700

    menambahkan deskripsi jsonld

--- components/Seo/JsonLDVideo.js
+++ components/Seo/JsonLDVideo.js
@@ -1,6 +1,5 @@
 import { SITEMAP } from "../../config"
 
-<<<<<<< HEAD
 // import { BASE_URL, SHARE_BASE_URL } from '../../config'
 const oneSegment = 'm.rctiplus.com';
 export default function JsonLDVideo ({content, isProgram}) {
@@ -38,68 +37,6 @@ export default function JsonLDVideo ({content, isProgram}) {
         // "duration": "PT1M54S",
         "contentUrl": `${oneSegment}${content?.asPath}`,
         // "embedUrl": `${oneSegment}${content?.asPath}`,
-=======
-export default function JsonLDVideo (props) {
-
-  const { seoContent, stream } = props
-  const rplusUrl = 'https://m.rctiplus.com'
-
-  if (!seoContent || !stream) return null
-  if (!seoContent.jsonLD) return null
-  if (!stream.detail) return null
-  if (!stream.detail.name_code) return null
-
-  const channel = (title) => {
-    switch (title) {
-      case "rcti":
-        return {
-          name: "RCTI",
-          interactionCount: "4927563",
-          ratingCount: "841",
-          sameAs: [
-            "https://www.rcti.tv/",
-            "https://www.google.com/search?q=RCTI&kponly&kgmid=/m/0824qb",
-            "https://id.wikipedia.org/wiki/RCTI",
-            "https://www.wikidata.org/wiki/Q5257835"
-          ]
-        }
-      case "mnctv":
-        return {
-          name: "MNC TV",
-          interactionCount: "1085711",
-          ratingCount: "721",
-          sameAs: [
-            "https://mnctv.com/",
-            "https://www.google.com/search?q=mnctv&kponly&kgmid=/m/0dvf5k",
-            "https://id.wikipedia.org/wiki/MNCTV",
-            "https://www.wikidata.org/wiki/Q6683165",
-          ]
-        }
-      case "gtv":
-        return {
-          name: "Global TV (GTV)",
-          interactionCount: "76242",
-          ratingCount: "693",
-          sameAs: [
-            "https://www.gtv.id/",
-            "https://www.google.com/search?q=global+tv&kponly&kgmid=/m/0b7bnq",
-            "https://id.wikipedia.org/wiki/GTV_(Indonesia)",
-            "https://www.wikidata.org/wiki/Q4201809",
-          ]
-        }
-      case "inews":
-        return {
-          name: "iNews",
-          interactionCount: "741225",
-          ratingCount: "803",
-          sameAs: [
-            "https://www.inews.id/",
-            "https://www.google.com/search?q=iNews&kponly&kgmid=/m/0gh85nz",
-            "https://id.wikipedia.org/wiki/INews",
-            "https://www.wikidata.org/wiki/Q4213609"
-          ]
-        }
->>>>>>> development
     }
   }
 
@@ -143,7 +80,6 @@ export default function JsonLDVideo (props) {
     </>
   )
 }
-<<<<<<< HEAD
 
 const channel = (title) => {
   if(title === 'rcti') {
@@ -160,5 +96,3 @@ const channel = (title) => {
   }
   return title
 }
-=======
->>>>>>> development
