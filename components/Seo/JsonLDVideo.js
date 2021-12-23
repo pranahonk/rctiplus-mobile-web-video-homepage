aa47bce9ae3aaf96bbabb9761e17062ab222a2e3
Feri_Romadhona93 <feri.romadhona@mncgroup.com>
2021-12-23 10:57:01 +0700

    jsonld update tambah sameas

--- components/Seo/JsonLDVideo.js
+++ components/Seo/JsonLDVideo.js
@@ -2,30 +2,26 @@
 // import { BASE_URL, SHARE_BASE_URL } from '../../config'
 const oneSegment = 'm.rctiplus.com';
 export default function JsonLDVideo ({content, isProgram}) {
-    // console.log('Meta seo: ',content)
-    export default function JsonLDVideo ({content, isProgram}) {
-      // console.log('Meta seo: ',content)
-    const structurData = {
-      "@context": "https://schema.org",
-      "@type": "VideoObject",
-      "contentURL": `${oneSegment}${content?.asPath}`,
-      "description": `${content?.description}`,
-      // "duration": "PT37M14S",
-      "embedUrl": `${oneSegment}${content?.asPath}`,
-      "expires": `${content?.expires || '1 Januari 9999'}`,
-      "interactionCount": "4756",
-      "name": `Live ${channel(content?.title)} livestream!`,
-      "thumbnailUrl": `${content?.thumbnailUrl}`,
-      "uploadDate": content?.startDate || "2018-10-27T14:00:00+00:00",
-      "publication": [
-        {
-          "@type": "BroadcastEvent",
-          "isLiveBroadcast": true,
-          "startDate": content?.startDate || '1 Januari 1979',
-          "endDate": content?.endDate || '1 Januari 9999'
-        }
-      ]
+  // console.log('Meta seo: ',content)
+const structurData = {
+  "@context": "https://schema.org",
+  "@type": "VideoObject",
+  "contentURL": `${oneSegment}${content?.asPath}`,
+  "description": `${content?.description}`,
+  "name": `Live ${content?.title} `,
+  "thumbnailUrl": `${content?.thumbnailUrl}`,
+  "sameAs": `${content?.sameAs}`,
+  "uploadDate": content?.startDate || "2018-10-27T14:00:00+00:00",
+  "publication":
+    {
+      "@type": "BroadcastEvent",
+      "isLiveBroadcast": true,
+      "startDate": content?.startDate || '1 Januari 1979',
+      "endDate": content?.endDate || '1 Januari 9999',
+      "sameAs": `${content?.sameAs_arr}`,
     }
+
+}
   const structurDataProgram = {
         "@context": "https://schema.org",
         "@type": "VideoObject",
