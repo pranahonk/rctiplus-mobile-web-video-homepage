341ff18e9e2e87b6c1a49936aef691d08f824583
Feri_Romadhona93 <feri.romadhona@mncgroup.com>
2022-01-07 15:49:59 +0700

    handle jika extra, episode, clip null

--- pages/programs.js
+++ pages/programs.js
@@ -679,7 +679,7 @@ class Index extends React.Component {
     const { query } = this.props.router
     const programDetail = this.props.server['program-detail']
 
-    if (programClip == null ||((programClip.data.length === 0) && programDetail.data.id !== +query.id) && !bookmark) return null
+    if (((programClip.data.length === 0) && programDetail.data.id !== +query.id) && !bookmark) return null
 
     const pagination = {
       ...programClip.meta.pagination,
