009943021c2bbb62af3064b0eb8f52bc1db818bd
prana.wijaya <prana.wijaya@mncgroup.com>
2022-01-21 18:02:08 +0700

    fixing hot video

--- graphql/queries/hot-video.js
+++ graphql/queries/hot-video.js
@@ -77,47 +77,46 @@ export const GET_HOT_VIDEO = (page = 1, pageSize= 100, page_lineups = 1, pageSiz
 };
 
 
-export const GET_HOT_VIDEO_PAGINATIONS = (page = 1, pageSize= 100, lineup_id= 88) => {
+export const GET_HOT_VIDEO_PAGINATIONS_UPDATE = (page, page_size, lineup_id) =>{
   return gql`
-    query {
-      lineup_contents(page: ${page}, page_size: ${pageSize}, lineup_id: ${lineup_id}){
-        data{
-          content_type_detail{
-            ... on ContentTypeHOTVideo {
-              detail{
-                data{
-                    thumbnail
-                    id
-                    views
-                    deeplink
-                    permalink
-                    contestant{
-                        nick_name
-                        thumbnail
-                        display_name
-                        email
-                        phone_code
-                        phone_number
-                    }
-                }
+      query {
+          lineup_contents(page: ${page}, page_size: ${page_size}, lineup_id: ${lineup_id}) {
+              data {
+                  content_type_detail {
+                      ... on ContentTypeHOTVideo {
+                          detail {
+                              data {
+                                  thumbnail
+                                  id
+                                  views
+                                  deeplink
+                                  permalink
+                                  contestant {
+                                      nick_name
+                                      thumbnail
+                                      display_name
+                                      email
+                                      phone_code
+                                      phone_number
+                                  }
+                              }
+                          }
+                      }
+                  }
               }
-
-            }
+              meta {
+                  assets_url
+                  image_path
+                  pagination {
+                      current_page
+                      per_page
+                      total
+                      total_page
+                  }
+                  video_path
+              }
           }
-
-        }
-        meta{
-          assets_url
-          image_path
-          pagination{
-            current_page
-            per_page
-            total
-            total_page
-          }
-          video_path
-        }
       }
-    }
+
   `
 }
