895acbcbcd019e929709c0fdee0fa654481a3833
andrumahardi <andru.mahardi@mncgroup.com>
2022-02-07 14:56:51 +0700

    add news count

--- components/Includes/Gallery/Carousel_v2.js
+++ components/Includes/Gallery/Carousel_v2.js
@@ -1,16 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import Router, { withRouter } from 'next/router';
-import { connect } from 'react-redux';
-import contentActions from '../../../redux/actions/contentActions';
-import { Carousel } from 'react-responsive-carousel';
-import Img from 'react-image';
+import React, { useEffect, useState } from 'react'
+import Router, { withRouter } from 'next/router'
+import { connect } from 'react-redux'
+import { Carousel } from 'react-responsive-carousel'
+import Img from 'react-image'
 
-import { RESOLUTION_IMG } from '../../../config';
-import { homeBannerEvent } from '../../../utils/appier';
+import contentActions from '../../../redux/actions/contentActions'
+import { newsCountViewDetail, newsCountViewTag } from "../../../redux/actions/newsCountView"
+import { RESOLUTION_IMG } from '../../../config'
+import { homeBannerEvent } from '../../../utils/appier'
 import { client } from "../../../graphql/client"
 import { GET_BANNERS } from "../../../graphql/queries/homepage" 
 
-import '../../../assets/scss/plugins/carousel/carousel.scss';
+import '../../../assets/scss/plugins/carousel/carousel.scss'
 
 function carouselBanner(props) {
     const placeholderImg = "/static/placeholders/placeholder_landscape.png"
@@ -34,10 +35,37 @@ function carouselBanner(props) {
     const goToProgram = (banner) => {
         sendTracker(homeBannerEvent, "homeBanner", banner)
 
+
+    if(banner.type === "news_tags"){
+        const params = {
+          'tag': banner.permalink.split('/')[6],
+        }
+  
+        props.newsCountViewTag(params)
+      }else if(banner.type === "news_detail"){
+        if(!Cookie.get('uid_ads')) {
+          Cookie.set('uid_ads', new DeviceUUID().get())
+        }
+        else{
+  
+          props.newsCountViewDetail(Cookie.get('uid_ads'), parseInt(banner.permalink.split('/')[6]))
+        }
+      }
+
         switch (banner.type) {
             case "url":
                 if (!banner.external_link) return
                 return Router.push(banner.external_link)
+            case "news_tags": {
+                const tag = banner.permalink.split('/')[6]
+                props.newsCountViewTag({ tag })
+                return
+            }
+            case "news_detail" : {
+                const id = banner.permalink.split('/')[6] || 0
+                props.newsCountViewDetail(new DeviceUUID().get(), +id)
+                return
+            }
             case "scan_qr":
                 return Router.push("/qrcode")
             default:
@@ -88,9 +116,9 @@ function carouselBanner(props) {
                         infiniteLoop
                         swipeable 
                         onSwipeEnd={(e) => {
-                            const swipedIndex = e.target.getAttribute('data-index');
+                            const swipedIndex = e.target.getAttribute('data-index')
                             if (banners[swipedIndex]) {
-                                const banner = banners[swipedIndex];
+                                const banner = banners[swipedIndex]
                                 sendTracker(homeBannerEvent, "homeBanner", banner)
                             }
                         }}>
@@ -116,7 +144,7 @@ function carouselBanner(props) {
             {props.children}
             <div style={{ position: 'absolute', bottom: -1.5, background: 'linear-gradient(180deg, #282828 9.89%, rgba(0, 0, 0, 0.0001) 100%)', transform: 'matrix(1, 0, 0, -1, 0, 0)', width: '100%', height: 136 }}></div>
         </div>
-    );
+    )
 }
 
-export default connect(state => state, contentActions)(withRouter(carouselBanner));
+export default connect(state => state, contentActions)(withRouter(carouselBanner))
