62d11e575e1d4c111a752659785a287838506285
prana.wijaya <prana.wijaya@mncgroup.com>
2022-03-10 15:06:37 +0700

    fixing loader in dev

--- components/lineups/news/HorizontalHastags.js
+++ components/lineups/news/HorizontalHastags.js
@@ -111,7 +111,7 @@ const HorizontalHastags = ({title, indexTag, id, data, ...props}) => {
   };
 
   return (
-    hastags?.data?.length < 1 ? (<div />) :
+      hastags?.data === undefined || hastags?.data?.length < 1 ? (<div />) :
         <div
           onTouchStart={e => onTouchStart(e)}
           onTouchEnd={e => onTouchEnd(e)}
