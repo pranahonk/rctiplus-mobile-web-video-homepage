6e81dc78ca935ee52f66c0da71fa1c235ade09f4
Randy <randywardhana37@gmail.com>
2022-07-08 20:46:16 +0700

    add custom props to bring ads path and ads id for sticky ads

--- components/Includes/Banner/StickyAds.js
+++ components/Includes/Banner/StickyAds.js
@@ -7,9 +7,15 @@ import '../../../assets/scss/components/sticky_ads.scss'
 import adsActions from '../../../redux/actions/adsActions'
 
 class StickyAds extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      adsPath: props.path ? props.path : process.env.MODE === "PRODUCTION" ? "/21865661642/PRO_MIDDLE_MOBILE" : "/21865661642/RC_MIDDLE_MOBILE",
+      adsId: props.id ? props.id : process.env.MODE === "PRODUCTION" ? "div-gpt-ad-1584677487159-0" : "div-gpt-ad-1584677577539-0"
+    }
+  }
+
   fetchAds(custParams) {
-    const slotName = process.env.MODE === "PRODUCTION" ? "/21865661642/PRO_MIDDLE_MOBILE" : "/21865661642/RC_MIDDLE_MOBILE"
-    const gptID = process.env.MODE === "PRODUCTION" ? "div-gpt-ad-1584677487159-0" : "div-gpt-ad-1584677577539-0"
     const targettingAdsData = custParams.concat({ 
       name: "logged_in", 
       value: String(Boolean(getUserAccessToken()))
@@ -17,7 +23,7 @@ class StickyAds extends React.Component {
     
     window.googletag = window.googletag || {cmd: []}
     googletag.cmd.push(function() {
-      googletag.defineSlot(slotName, [320, 50], gptID).addService(googletag.pubads())
+      googletag.defineSlot(this.state.adsPath, [320, 50], this.state.adsId).addService(googletag.pubads())
       
       targettingAdsData.forEach(({ name, value }) => {
         googletag.pubads().setTargeting(name, value)
@@ -47,8 +53,6 @@ class StickyAds extends React.Component {
   }
 
   render() {
-    const gptID = process.env.MODE === "PRODUCTION" ? "div-gpt-ad-1584677487159-0" : "div-gpt-ad-1584677577539-0"
-
     return (
       <div id="sticky-ads-container" className={`sticky-ads ${(this.props.sticky ? 'sticky-ads-on' : '')}`}>
         {this.props.sticky
@@ -63,7 +67,7 @@ class StickyAds extends React.Component {
         <div>
           <div className="sticky-ads-content">
             <center>
-            <div id={gptID} style={{
+            <div id={this.state.adsId} style={{
               width: 320,
               height: 50
             }}>
