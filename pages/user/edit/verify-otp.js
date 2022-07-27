afbf2aa925c3257d28dbc12d11cabdd097cf8300
Pahmi Alifya Bahri <pahmi.bahri@mncgroup.com>
2022-07-27 19:58:04 +0700

    handle phone code

--- pages/user/edit/verify-otp.js
+++ pages/user/edit/verify-otp.js
@@ -81,12 +81,21 @@ class VerifyOtp extends React.Component {
         
     }
 
+    isPhoneCode(code){
+        if(!code) return null
+        if(this.state.username.substring(0, 2)?.toString() === code?.toString()) return null
+
+        return code
+    }
+    
+
     onChangeOtp(otp) {
+        this.isPhoneCode(this.props.registration.phone_code)
         this.setState({ otp: otp, is_submitting: otp && otp.length >= 6 }, () => {
             this.props.setOtp(this.state.otp);
             if (this.state.is_submitting) {
                 console.log(this.props)
-                this.props.verifyOtp(this.state.username, this.state.otp)
+                this.props.verifyOtp(this.state.username, this.state.otp, this.isPhoneCode(this.props.registration.phone_code))
                     .then(response => {
                         if (response.status === 200) {
                             switch (response.data.status.code) {
