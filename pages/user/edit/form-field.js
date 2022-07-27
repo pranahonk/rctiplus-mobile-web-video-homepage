de29f79c32a9bc0773aacfe8f6617b902d57764c
Pahmi Alifya Bahri <pahmi.bahri@mncgroup.com>
2022-07-27 16:54:34 +0700

    fixing error

--- pages/user/edit/form-field.js
+++ pages/user/edit/form-field.js
@@ -185,8 +185,8 @@ class FormField extends React.Component {
     }
     componentDidMount() {
         // console.log(this.props)
-        this.setState({ codeCountry: (this.props.user && this.props.user.data && this.props.user.data.country_code === '') ||
-                                    (this.props.user && this.props.user.data && this.props.user.data.country_code === null) ? this.state.codeCountry : this.props.user.data.country_code,
+        this.setState({ codeCountry: (this.props.user && this.props.user.data && this.props.user.data?.country_code === '') ||
+                                    (this.props.user && this.props.user.data && this.props.user.data?.country_code === null) ? this.state.codeCountry : this.props?.user?.data?.country_code,
                         phone_code: (this.props.user && this.props.user.data && this.props.user.data.phone_code === '') ||
                                     (this.props.user && this.props.user.data && this.props.user.data.phone_code === null) ? this.state.phone_code : this.props.user.data.phone_code});
         console.log(this.props.user && this.props.user.data)
