f9499b10c4436b0392a76c17829a636a3d27a751
prana.wijaya <prana.wijaya@mncgroup.com>
2022-08-08 16:57:44 +0700

    back to react component

--- components/Includes/Banner/StickyAds.js
+++ components/Includes/Banner/StickyAds.js
@@ -1,21 +1,15 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from 'react'
+import { connect } from 'react-redux'
 
-import { getUserAccessToken } from "../../../utils/cookie";
-import '../../../assets/scss/components/sticky_ads.scss';
+import { getUserAccessToken } from "../../../utils/cookie"
+import '../../../assets/scss/components/sticky_ads.scss'
 
-import adsActions from '../../../redux/actions/adsActions';
+import adsActions from '../../../redux/actions/adsActions'
 
 class StickyAds extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      adsPath: props.path ? props.path : process.env.MODE === "PRODUCTION" ? "/21865661642/PRO_MIDDLE_MOBILE" : "/21865661642/RC_MIDDLE_MOBILE",
-      adsId: props.id ? props.id : process.env.MODE === "PRODUCTION" ? "div-gpt-ad-1584677487159-0" : "div-gpt-ad-1584677577539-0"
-    }
-  }
-
   fetchAds(custParams) {
+    const slotName = process.env.MODE === "PRODUCTION" ? "/21865661642/PRO_MIDDLE_MOBILE" : "/21865661642/RC_MIDDLE_MOBILE"
+    const gptID = process.env.MODE === "PRODUCTION" ? "div-gpt-ad-1584677487159-0" : "div-gpt-ad-1584677577539-0"
     const targettingAdsData = custParams.concat({
       name: "logged_in",
       value: String(Boolean(getUserAccessToken()))
@@ -23,7 +17,7 @@ class StickyAds extends React.Component {
 
     window.googletag = window.googletag || {cmd: []}
     googletag.cmd.push(function() {
-      googletag.defineSlot(this.state.adsPath, [320, 50], this.state.adsId).addService(googletag.pubads())
+      googletag.defineSlot(slotName, [320, 50], gptID).addService(googletag.pubads())
 
       targettingAdsData.forEach(({ name, value }) => {
         googletag.pubads().setTargeting(name, value)
@@ -39,7 +33,7 @@ class StickyAds extends React.Component {
       })
       googletag.enableServices()
     })
-    googletag.cmd.push(function() { googletag.display(this.state.adsId) })
+    googletag.cmd.push(function() { googletag.display(gptID) })
   }
 
   componentDidMount() {
@@ -53,25 +47,27 @@ class StickyAds extends React.Component {
   }
 
   render() {
+    const gptID = process.env.MODE === "PRODUCTION" ? "div-gpt-ad-1584677487159-0" : "div-gpt-ad-1584677577539-0"
+
     return (
       <div id="sticky-ads-container" className={`sticky-ads ${(this.props.sticky ? 'sticky-ads-on' : '')}`}>
         {this.props.sticky
-        ? (
-          <div
-            className="ads-close-btn"
-            onClick={() => this.props.toggleAds(false)}></div>
-        )
-        : null}
+          ? (
+            <div
+              className="ads-close-btn"
+              onClick={() => this.props.toggleAds(false)}></div>
+          )
+          : null}
         <script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
 
         <div>
           <div className="sticky-ads-content">
             <center>
-            <div id={this.state.adsId} style={{
-              width: 320,
-              height: 50
-            }}>
-            </div>
+              <div id={gptID} style={{
+                width: 320,
+                height: 50
+              }}>
+              </div>
             </center>
           </div>
         </div>
