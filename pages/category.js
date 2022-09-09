14d091d2f76cfd0bfd5c83b26f658c6057bbca13
prana.wijaya <prana.wijaya@mncgroup.com>
2022-09-09 15:19:13 +0700

    handle if gpt empty

--- pages/category.js
+++ pages/category.js
@@ -310,9 +310,9 @@ function Category (props) {
                       }
 
 
-                      const adsContents = document.getElementById(gpt.div_gpt).childNodes;
+                      const adsContents = document.getElementById(gpt.div_gpt)?.childNodes;
 
-                      if (adsContents.length > 0) {
+                      if (adsContents?.length > 0) {
                         if (adsContents[0].tagName == 'SCRIPT') {
                           const stickyAds = gpt.div_gpt.childNodes;
                           if (stickyAds) {
